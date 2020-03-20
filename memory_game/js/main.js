7dce937e6512d585fec9f0d0ac96b0699515004d
RickD <ricky.pleace@gmail.com>
2020-03-20 18:11:39 +1100

    Created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,19 @@
 console.log("Up and running!");
 
-let cardOne = 'queen';
-let cardTwo = 'queen';
-let cardThree = 'king';
-let cardFour = 'king';
+const cards = ["queen","queen","king","king"];
+const cardsInPlay = [];
 
-console.log('User flipped ' + cardTwo);
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+let cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert('You found a match!');
+	}
+	else {
+		alert('Sorry, try again.');
+	}
+}
