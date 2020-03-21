0e20d59c93d80fa2a348a3e8949b41265f3d5373
RickD <ricky.pleace@gmail.com>
2020-03-21 12:24:09 +1100

    completed game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -35,13 +35,24 @@ function checkForMatch() {
     }
 }
 
-function flipCard(cardId) {
+function flipCard() {
+    let cardId = this.getAttribute('data-id');
     console.log("User flipped " + cards[cardId].rank);
     cardsInPlay.push(cards[cardId].rank);
     console.log(cards[cardId].suit);
     console.log(cards[cardId].cardImage);
+    this.setAttribute('src', cards[cardId].cardImage);
     checkForMatch();
 }
 
-flipCard(2);
-flipCard(0);
+function createBoard() {
+	for (let i=0; i<cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);	
+	}
+}
+
+createBoard();
