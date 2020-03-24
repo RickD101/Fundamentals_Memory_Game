4d4f51625c70d0541034285c75207be340f79d73
RickD <ricky.pleace@gmail.com>
2020-03-24 20:31:30 +1100

    added random card selection

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -80,7 +80,8 @@ cardImage: "images/png/7_of_hearts.png"
 rank: "seven",
 suit: "diamonds",
 cardImage: "images/png/7_of_diamonds.png"
-},{
+},
+{
 rank: "six",
 suit: "hearts",
 cardImage: "images/png/6_of_hearts.png"
@@ -119,7 +120,8 @@ cardImage: "images/png/3_of_hearts.png"
 rank: "three",
 suit: "diamonds",
 cardImage: "images/png/3_of_diamonds.png"
-},{
+},
+{
 rank: "two",
 suit: "hearts",
 cardImage: "images/png/2_of_hearts.png"
@@ -208,7 +210,8 @@ cardImage: "images/png/7_of_spades.png"
 rank: "seven",
 suit: "clubs",
 cardImage: "images/png/7_of_clubs.png"
-},{
+},
+{
 rank: "six",
 suit: "spades",
 cardImage: "images/png/6_of_spades.png"
@@ -247,7 +250,8 @@ cardImage: "images/png/3_of_spades.png"
 rank: "three",
 suit: "clubs",
 cardImage: "images/png/3_of_clubs.png"
-},{
+},
+{
 rank: "two",
 suit: "spades",
 cardImage: "images/png/2_of_spades.png"
@@ -263,8 +267,17 @@ let cardsInPlay = [];
 let cardsFlipped = [];
 let cardsNotFlipped = [];
 let score = 0;
-let delay = 150;
-let cardNumber = 52;
+let bonus = 1;
+let delay = 1250;
+let cardNumber = 18;
+let cardSelection = [];
+let board;
+let cardMatch =[];
+
+let cardSelect =[];
+for (i=0; i<cards.length; i++) {
+	cardSelect[i] = i;
+}
 
 function checkForMatch() {	
 	if (cardsInPlay.length === 2) {
@@ -272,13 +285,15 @@ function checkForMatch() {
 	    if (cardsInPlay[0] === cardsInPlay[1]) {
 		    document.getElementById('notification').textContent = 'Found a match!';
 		    setTimeout(resetBar, delay);
-		    score += 1;
+		    score += 1*bonus;
+		    bonus = bonus*2;
 	    }
 	    else {
 		    document.getElementById('notification').textContent = 'Try again!';
 		    setTimeout(resetFlip, delay);
 		    setTimeout(resetBar, delay);
 		    score -= 1;
+		    bonus = 1;
 	    }
 	    document.getElementById('score').textContent = 'Score: ' + score;
 	    cardsInPlay = [];
@@ -300,49 +315,80 @@ function flipCard() {
 
 function blockInput() {
 	for (let i=0; i<cardNumber; i++) {
-		document.getElementById(i).removeEventListener('click', flipCard);
+		document.getElementById(cardSelection[i]).removeEventListener('click', flipCard);
 	}
 }
 
 function restoreInput() {
-	cardsNotFlipped = document.querySelectorAll("img[src='images/back.png'");
+	cardsNotFlipped = document.querySelectorAll("img[src='images/back2.png'");
 	for (let i=0; i<cardsNotFlipped.length; i++) {
 		cardsNotFlipped[i].addEventListener('click', flipCard);
 	}
 }
 
 function createBoard() {
-	for (let i=0; i<cardNumber; i++) {
+
+	let a = [];
+	let b = [];
+
+	for (let i=0; i<cardNumber/2; i++) {
+		
+		cardMatch = [];
+
+		a = cardSelect[Math.floor(Math.random()*cardSelect.length)];
+		cardSelect.splice(cardSelect.indexOf(a), 1);
+
+		for (let i=0; i<cardSelect.length; i++) {
+			if (cards[cardSelect[i]].rank === cards[a].rank) {
+				cardMatch.push(cardSelect[i]);
+			}
+		}
+		
+		b = cardMatch[Math.floor(Math.random()*cardMatch.length)];
+		cardSelect.splice(cardSelect.indexOf(b), 1);
+
+		cardSelection.push(a, b);
+	}
+
+	shuffleArray(cardSelection);
+
+	for (let i=0; i<cardSelection.length; i++) {
 		let cardElement = document.createElement('img');
-		cardElement.setAttribute('src', 'images/back.png');
-		cardElement.setAttribute('id', i);
-		cardsNotFlipped.push(i);
+		cardElement.setAttribute('src', 'images/back2.png');
+		cardElement.setAttribute('id', cardSelection[i]);
 		cardElement.addEventListener('click', flipCard);
 		document.getElementById('game-board').appendChild(cardElement);	
 	}
+
 }
 
 function resetFlip() {
 	let card1 = document.getElementById(cardsFlipped[cardsFlipped.length -1]);
-	card1.setAttribute('src', 'images/back.png');
+	card1.setAttribute('src', 'images/back2.png');
 	card1.addEventListener('click', flipCard);
 	let card2 = document.getElementById(cardsFlipped[cardsFlipped.length -2]);
-	card2.setAttribute('src', 'images/back.png');
+	card2.setAttribute('src', 'images/back2.png');
 	card2.addEventListener('click', flipCard);
 	cardsFlipped.pop();
 	cardsFlipped.pop();
 }
 
 function resetBoard() {
-	let board = document.getElementById('game-board');
+	
+	board = document.getElementById('game-board');
 	for (let i=0; i<cardNumber; i++) {
 		board.removeChild(board.childNodes[0]);
 	}
 	score = 0;
+	bonus = 1;
 	document.getElementById('score').textContent = 'Score: ' + score;
 	cardsInPlay = [];
 	cardsFlipped = [];
 	cardsNotFlipped = [];
+	cardSelection = [];
+	for (i=0; i<cards.length; i++) {
+		cardSelect[i] = i;
+	}
 	resetBar();
 	createBoard();
 }
@@ -355,7 +401,14 @@ function gameOver() {
 	document.getElementById('notification').textContent = 'Game Over.';
 }
 
-createBoard();
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
 
-document.getElementById('reset').addEventListener('click', resetBoard);
+createBoard();
 
